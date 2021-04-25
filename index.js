89413d2fac4a4f2c0b3d86db86ca4c59169851a7
Raghda  Muayad <therose000777@gmail.com>
2021-04-25 13:52:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var newHeader = document.createElement("h1");
 //function myFunction() {
   //document.getElementById("victory").innerHTML = h1;
 //}
+document.getElementById("victory").innerHTML = "New text!";
 var node = document.createTextNode("YOUR-NAME is the champion");
 newHeader.appendChild(node);
 var element = document.getElementById("victory");
