49e753018618891c29862ed709523447743eb8bb
Raghda  Muayad <therose000777@gmail.com>
2021-04-25 13:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here!
 var newHeader = document.createElement("h1");
 function myFunction() {
-  document.getElementById("victory").innerHTML = x;
+  document.getElementById("victory").innerHTML = h1;
 }
 var node = document.createTextNode("YOUR-NAME is the champion");
 newHeader.appendChild(node);
